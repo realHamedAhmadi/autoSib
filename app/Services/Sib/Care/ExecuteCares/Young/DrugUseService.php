2872257164719e0653d4fcec7570af9f59fe1a3a
Hamed Ahmadi <Hamed4Ahmadi@gmail.com>
2026-08-01 11:58:00 -0400

    feat: set handle for young: drugUseService

--- app/Services/Sib/Care/ExecuteCares/Young/DrugUseService.php
+++ app/Services/Sib/Care/ExecuteCares/Young/DrugUseService.php
@@ -13,6 +13,31 @@ class DrugUseService extends BaseYoungCareService
 {
     public function handle(CompletedCareData $olderCareDate, SibUserInfo $userInfo, ?UserPayload $payload): array
     {
-        // TODO: Implement handle() method.
+        $answers=[];
+        $answers[]=[
+            "Id_Condition"=>29736,
+            "Answer"=>1,
+            "PostProcessAnswer"=> 1
+        ];
+        $ids=[
+            29736=>1,
+            29480=>115401,
+            29481=>115404,
+            29482=>115407,
+            29483=>115410,
+            29484=>115413,
+            29485=>115416,
+            29486=>115419,
+            28136=>0,
+            29565=>0,
+            ];
+        foreach ($ids as $id=>$ans){
+            $answers[]=[
+                "Id_Condition"=>$id,
+                "Answer"=>$ans,
+                "PostProcessAnswer"=> $ans
+            ];
+        }
+        return $answers;
     }
 }
