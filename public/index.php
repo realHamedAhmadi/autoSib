f83bcc5d6b2e8cf9ecc3f6278052706ff147efb2
Hamed Ahmadi <Hamed4Ahmadi@gmail.com>
2026-08-08 11:07:59 -0400

    fix: suppress PHP errors conditionally for 8.5+

--- public/index.php
+++ public/index.php
@@ -4,6 +4,18 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+/**
+ * Suppress specific errors for PHP 8.5 and above
+ */
+if (version_compare(PHP_VERSION, '8.5.0', '>=')) {
+    // Disable displaying errors to the screen
+    ini_set('display_errors', '0');
+    ini_set('display_startup_errors', '0');
+
+    // Report only fatal errors, ignoring Warnings, Notices, and Deprecated messages
+    error_reporting(E_ERROR | E_PARSE | E_CORE_ERROR | E_COMPILE_ERROR);
+}
+
 // Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
