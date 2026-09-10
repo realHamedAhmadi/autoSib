7d21cd58ae77a1849bbce58fb0ea878a49a3b15c
Hamed Ahmadi <Hamed4Ahmadi@gmail.com>
2026-09-10 12:38:58 -0400

    feat: add max user care & care sleep time cols migration for users table

---
+++ database/migrations/2026_09_10_200053_add_max_user_care_and_care_sleep_time_cols_to_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedInteger('max_user_care')->nullable();
+            $table->unsignedTinyInteger('care_sleep_time')->default(15);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('max_user_care');
+            $table->dropColumn('care_sleep_time');
+        });
+    }
+};
